231cf213c0e002da691ee0d86558600b309dec59
Shota-Yukawa <71016455+Shota-Yukawa@users.noreply.github.com>
2023-09-19 22:59:48 +0900

    first commit

---
+++ src/test/java/com/ah/tablesync/TablesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ah.tablesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
